12eebc6bbd56bccdffdb4496db6016b5889b4ee0
YimingRen111 <111114604+YimingRen111@users.noreply.github.com>
2023-10-29 17:58:29 +1100

    Add files via upload

---
+++ matchingsystem/src/main/java/com/kob/matchingsystem/controller/MatchingController.java
@@ -0,0 +1,34 @@
+package com.kob.matchingsystem.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import com.kob.matchingsystem.service.MatchingService;
+
+import java.util.Objects;
+
+@RestController
+public class MatchingController{
+@Autowired
+    private MatchingService matchingService;
+
+    @PostMapping("/player/add/")
+    public String addPlayer(@RequestParam MultiValueMap<String, String> data){
+        Integer userId = Integer.parseInt(Objects.requireNonNull(data.getFirst("user_id")));
+        Integer rating = Integer.parseInt(Objects.requireNonNull(data.getFirst("rating")));
+        Integer botId = Integer.parseInt(Objects.requireNonNull(data.getFirst("bot_id")));
+        Integer gameId = Integer.parseInt(Objects.requireNonNull(data.getFirst("game_id")));
+        return matchingService.addPlayer( userId, rating,botId , gameId );
+
+    }
+
+    @PostMapping("/player/remove/")
+    public String removePlayer(@RequestParam MultiValueMap<String, String> data){
+        Integer userId = Integer.parseInt(Objects.requireNonNull(data.getFirst("user_id")));
+        return matchingService.removePlayer(userId);
+    }
+
+
+}
---
+++ matchingsystem/src/main/java/com/kob/matchingsystem/service/impl/MatchingServiceImpl.java
@@ -0,0 +1,23 @@
+package com.kob.matchingsystem.service.impl;
+
+import com.kob.matchingsystem.service.MatchingService;
+import com.kob.matchingsystem.service.impl.utils.MatchingPool;
+import org.springframework.stereotype.Service;
+
+@Service
+public class MatchingServiceImpl implements MatchingService {
+    public final static MatchingPool matchingPool = new MatchingPool();
+    @Override
+    public String addPlayer(Integer userId, Integer rating, Integer botId , Integer gameId) {
+        System.out.println("addPlayer" + userId + " " + rating+" "+botId + " " + gameId) ;
+        matchingPool.addPlayers(userId, rating,botId , gameId);
+        return "add player success";
+    }
+    @Override
+    public String removePlayer(Integer userId) {
+        System.out.println("removePlayer");
+        matchingPool.removePlayers(userId);
+        return "remove player success";
+    }
+
+}
